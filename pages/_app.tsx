c9bad0eab34cc832c6c247b7ddfe633d4e83a575
Manoj Santra <santramanoj1997@gmail.com>
2022-05-08 17:59:36 +0530

    final commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,8 @@ import {ThemeProvider} from "next-themes"
 function MyApp({ Component, pageProps }) {
   return(
     <ThemeProvider attribute="class">
-      <div className="grid grid-cols-12 gap-6  pl-2 pr-2 px-2 md:px-16  lg:px-20 lg:mt-8 mt-5 mx-auto pb-4 ">
-        <div className="lg:col-span-3 shadow-custom-light dark:shadow-custom-dark  bg-white dark:bg-dark-600 rounded-2xl  col-span-12 p-4 ">
+      <div className="grid grid-cols-12 gap-6  pl-2 pr-2 px-2 md:px-16  lg:px-20 lg:mt-8 mt-5 mx-auto pb-4 h-full">
+        <div className="lg:col-span-3 shadow-custom-light dark:shadow-custom-dark  bg-white dark:bg-dark-600 rounded-2xl  col-span-12 p-4 h-full ">
           <Sidebar/>
         </div>
         <div
